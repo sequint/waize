ece34a7f3c55c84d11f8df04d836bbc8ef831b1f
Steven Quintana <steve.e.quint@gmail.com>
2022-03-18 08:56:35 -0700

    Import mui basline and surround App comp with the element in order to reset css for the app

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import CssBaseline from '@mui/material/CssBaseline'
 import App from './app/App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline>
+      <App />
+    </CssBaseline>
   </React.StrictMode>,
   document.getElementById('root')
 )
