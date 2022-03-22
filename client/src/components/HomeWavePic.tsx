a4972c3a8a45c0ca40849fdd45f86f655b569dc2
Steven Quintana <steve.e.quint@gmail.com>
2022-03-22 10:43:48 -0700

    Create a wave picture component that spans the home screen

---
+++ client/src/components/HomeWavePic.tsx
@@ -0,0 +1,14 @@
+import wavePic from '../images/dashboardWave.jpeg'
+import './HomeWavePic.css'
+
+const HomeWavePic = () => {
+
+
+  return (
+    <>
+      <img src={wavePic} alt="waves with waize generator" className="homeWavePic" />
+    </>
+  )
+}
+
+export default HomeWavePic
