87174ad74dd2b982f2b929c5b931a2ad2d511af8
Steven Quintana <steve.e.quint@gmail.com>
2022-03-23 12:39:31 -0700

    Delete useEffect and speed dial icon imports

--- client/src/components/Camera.tsx
+++ client/src/components/Camera.tsx
@@ -1,7 +1,6 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Box from '@mui/material/Box'
 import SpeedDial from '@mui/material/SpeedDial'
-import SpeedDialIcon from '@mui/material/SpeedDialIcon'
 import SpeedDialAction from '@mui/material/SpeedDialAction'
 import CameraAltRoundedIcon from '@mui/icons-material/CameraAltRounded'
 import VideoCameraBackRoundedIcon from '@mui/icons-material/VideoCameraBackRounded'
