b1674ddb32201b313c75e93ee1a65efcc20d8312
Steven Quintana <steve.e.quint@gmail.com>
2022-03-25 08:42:52 -0700

    Create a prompt for initiate camera based on camera type state

--- client/src/components/Camera.tsx
+++ client/src/components/Camera.tsx
@@ -7,23 +7,36 @@ import VideoCameraBackRoundedIcon from '@mui/icons-material/VideoCameraBackRound
 import './Camera.css'
 
 const cameraOptions = [
-  { icon: <CameraAltRoundedIcon />, name: 'Photo' },
-  { icon: <VideoCameraBackRoundedIcon />, name: 'Video' }
+  {
+    icon: <CameraAltRoundedIcon />,
+    name: 'Photo',
+    prompt: 'Click anywhere to take a photo'
+  },
+  {
+    icon: <VideoCameraBackRoundedIcon />,
+    name: 'Video',
+    prompt: 'Click anywhere to start recording'
+  }
 ]
 
 const Camera = () => {
   const [cameraType, setCameraType] = useState({
     icon: cameraOptions[1].icon,
-    name: cameraOptions[1].name
+    name: cameraOptions[1].name,
+    prompt: cameraOptions[1].prompt
   })
 
+
+
   return (
-    <>
+    <div className="cameraContainer">
+      <h4 className="cameraPrompt">{cameraType.prompt}</h4>
       <Box sx={{ transform: 'translateZ(0px)', flexGrow: 1 }} className="optionsBox">
         <SpeedDial
           ariaLabel="SpeedDial basic example"
           sx={{ position: 'absolute', bottom: 16, right: 16 }}
           icon={cameraType.icon}
+          className="speedDialColor"
         >
           {cameraOptions.map((option) => (
             <SpeedDialAction
@@ -31,12 +44,12 @@ const Camera = () => {
               icon={option.icon}
               tooltipTitle={option.name}
               // On choice click, set cameraType state object to current object in the cameraOptions list
-              onClick={() => setCameraType({icon: option.icon, name: option.name})}
+              onClick={() => setCameraType({icon: option.icon, name: option.name, prompt: option.prompt})}
             />
           ))}
         </SpeedDial>
       </Box>
-    </>
+    </div>
   )
 }
 
