7a0a747705becd2542c38e451393bd3b20483982
Steven Quintana <steve.e.quint@gmail.com>
2022-03-30 15:54:29 -0700

    Create new CapturedPhoto comp w/ general mui modal in it and add it to the index file

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,5 +1,6 @@
 export { default as About } from './About/About'
 export { default as Camera } from './Camera/Camera'
+export {default as CapturedPhoto } from './CapturedPhoto/CapturedPhoto'
 export { default as HomeTitle } from './HomeTitle/HomeTitle'
 export { default as HomeWavePic } from './HomeWavePic/HomeWavePic'
 export { default as ImageUpload } from './ImageUpload/ImageUpload'
