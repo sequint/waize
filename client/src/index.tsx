66daed93b2d27c9ddc90438fa1ec955a8ae7970a
Steven Quintana <steve.e.quint@gmail.com>
2022-03-30 16:50:32 -0700

    Wrap App comp in a reduex provider with the app's store passed as the store prop

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import CssBaseline from '@mui/material/CssBaseline'
+import store from './app/store'
+import { Provider } from 'react-redux'
 import App from './app/App'
 
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </CssBaseline>
   </React.StrictMode>,
   document.getElementById('root')
