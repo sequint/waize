c3ad1e17d489b5d4e9aaeb81bf16125742908c5d
Steven Quintana <steve.e.quint@gmail.com>
2022-03-31 07:39:36 -0700

    Add back provider around app component

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import CssBaseline from '@mui/material/CssBaseline'
+import { Provider } from 'react-redux'
+import {store} from './app/store'
 import App from './app/App'
 
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
+      
     </CssBaseline>
   </React.StrictMode>,
   document.getElementById('root')
