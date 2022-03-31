6c4ea7b84491157fc579f96650fadb3fe7a828a5
Steven Quintana <steve.e.quint@gmail.com>
2022-03-30 17:25:31 -0700

    Delete redux provider wrapper from the index file

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import CssBaseline from '@mui/material/CssBaseline'
-import store from './app/store'
-import { Provider } from 'react-redux'
 import App from './app/App'
 
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </CssBaseline>
   </React.StrictMode>,
   document.getElementById('root')
