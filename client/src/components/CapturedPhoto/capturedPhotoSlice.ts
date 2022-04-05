e1117ac49bd58f5fe573c587c473ceca79d1b921
Steven Quintana <steve.e.quint@gmail.com>
2022-04-05 11:22:56 -0700

    Create a new state var and reducers to hold the captured photos url state and a recuder to update it when a new photo is captured

--- client/src/components/CapturedPhoto/capturedPhotoSlice.ts
+++ client/src/components/CapturedPhoto/capturedPhotoSlice.ts
@@ -2,12 +2,14 @@ import { createSlice } from '@reduxjs/toolkit'
 
 // Define state types for CapturedPhoto
 interface CapturedPhotoState {
-  open: boolean
+  open: boolean,
+  photoURL: String
 }
 
 // Initialize state values
 const initialState: CapturedPhotoState = {
-  open: false  // Initializes modal view state
+  open: false,
+  photoURL: ''
 }
 
 export const capturedPhotoSlice = createSlice({
@@ -18,14 +20,19 @@ export const capturedPhotoSlice = createSlice({
     togglePhotoView: (state: any) => {
       state.open = !state.open
     },
+    updatePhotoURL: (state: any, url: any) => {
+      state.photoURL = url.payload
+      console.log(state.photoURL)
+    },
   },
 })
 
-// Export the toggle action function for the capturedPhotoSlice
-export const { togglePhotoView } = capturedPhotoSlice.actions
+// Export the actions for this slice
+export const { togglePhotoView, updatePhotoURL } = capturedPhotoSlice.actions
 
-// Export state value
+// Export state values
 export const selectPhotoView = (state: any) => state.capturedPhoto.open
+export const selectPhotoURL = (state: any) => state.capturedPhoto.photoURL
 
 // Export the default slice reducer
 export default capturedPhotoSlice.reducer
