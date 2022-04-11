b0d1604e2add3e32e170ca4a84d153f01a3a279a
Steven Quintana <steve.e.quint@gmail.com>
2022-04-11 10:48:40 -0700

    Create function that returns first row of 10 suared px boxes by dividing screen width by 10 and returning an array of box divs

--- client/src/components/ColorGrid/ColorGrid.tsx
+++ client/src/components/ColorGrid/ColorGrid.tsx
@@ -1,9 +1,22 @@
 import './ColorGrid.css'
 
+const displayColorBoxes: any = () => {
+  const photoWidth = window.innerWidth
+  const gridLength = photoWidth / 10
+  console.log(gridLength)
+
+  let photoBoxList = []
+  for (let length = 0; length < gridLength; length++) {
+    photoBoxList.push(<div className="box"></div>)
+  }
+
+  return photoBoxList
+}
+
 const ColorGrid = () => {
   return (
     <div className="grid">
-      
+      {displayColorBoxes()}
     </div>
   )
 }
