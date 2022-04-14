5ee213461a37a19eb030471a25c4072d551027cc
Steven Quintana <steve.e.quint@gmail.com>
2022-04-14 12:01:05 -0700

    Create a useEffect function that extracts canvas context and create the canvas rectangle on load

--- client/src/components/Canvas/Canvas.tsx
+++ client/src/components/Canvas/Canvas.tsx
@@ -1,15 +1,22 @@
-import { useRef } from 'react'
+import { useRef, useEffect } from 'react'
 
 const Canvas = (props: any) => {
 
-  // Create vars using useRef hook to access dom element, then get it's context
+  // Create vars using useRef hook to access dom element
   const canvasRef = useRef(null)
-  const canvas: any = canvasRef.current
-  const canvasContext = canvas.getContext('2d')
 
+  // On component load extract canvas context and create the rectangle (area)
+  useEffect(() => {
+    const canvas: any = canvasRef.current
+    const canvasContext = canvas.getContext('2d')
+    canvasContext.fillRect(0, 0, 10, 10)
+  }, [])
+
+
+  // Retrun the 10x10 px canvas
   return (
     <>
-      <canvas ref={canvasRef} {...props} />
+      <canvas ref={canvasRef} width={10} height={10} {...props} />
     </>
   )
 }
