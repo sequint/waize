cffde9ee93342de92fc033847cb1dbaaf38c7ed1
Steven Quintana <steve.e.quint@gmail.com>
2022-04-14 08:14:08 -0700

    Create vars using useRef hook to access dom element then get it's context

--- client/src/components/Canvas/Canvas.tsx
+++ client/src/components/Canvas/Canvas.tsx
@@ -2,11 +2,14 @@ import { useRef } from 'react'
 
 const Canvas = (props: any) => {
 
+  // Create vars using useRef hook to access dom element, then get it's context
   const canvasRef = useRef(null)
+  const canvas: any = canvasRef.current
+  const canvasContext = canvas.getContext('2d')
 
   return (
     <>
-      <canvas {...props} />
+      <canvas ref={canvasRef} {...props} />
     </>
   )
 }
