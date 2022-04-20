827a49b41862c369be3c8b9f8b5c10cce0c1a602
Steven Quintana <steve.e.quint@gmail.com>
2022-04-20 14:25:54 -0700

    Recreate constraints for user media, create a video element using ref and add srcObject to the element

--- client/src/components/Camera/Camera.tsx
+++ client/src/components/Camera/Camera.tsx
@@ -1,11 +1,25 @@
+import { useRef, useState } from 'react'
+import { useUserMedia } from '../../hooks/useUserMedia'
 import './Camera.css'
 
+// Set constraints for video stream from user midea data (switch to environment facing after testing)
+const videoConstraints = {
+  audio: false,
+  video: { facingMode: 'user' }
+}
+
 const Camera = () => {
+  // Create a video element using ref
+  const videoRef: any = useRef()
+  const mediaStream = useUserMedia(videoConstraints)
+
+  // If there is a video stream live, a video element set, and no src on the object - then set a src equal to the mediaStream
+  if (mediaStream && videoRef.current && !videoRef.current.srcObject) {
+    videoRef.current.srcObject = mediaStream
+  }
 
   return (
-    <div className="webcamContainer">
-
-    </div>
+    <></>
   )
 }
 
