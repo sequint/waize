9467d972fcbddc2d13170fd29e001de56ff78f85
Steven Quintana <steve.e.quint@gmail.com>
2022-04-20 09:23:22 -0700

    Begin updates to camera to make it more light by utilizing html mediadevices in component and shedding Webcam package

--- client/src/components/Camera/Camera.tsx
+++ client/src/components/Camera/Camera.tsx
@@ -1,4 +1,5 @@
 import Webcam from 'react-webcam'
+import { useState } from 'react'
 import { useAppDispatch } from '../../app/hooks'
 import { togglePhotoView, updatePhotoURL } from '../CapturedPhoto/capturedPhotoSlice'
 import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked'
@@ -7,15 +8,32 @@ import './Camera.css'
 
 const Camera = () => {
   const dispatch = useAppDispatch()
+  const [ media, setMedia ] = useState({})
 
   const videoConstraints = {
     facingMode: "user"
   }
 
+  // polyfill based on https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+  const getMediaDevices = () => {
+    console.log('In function')
+    if (typeof window === 'undefined') { return }
+
+    // Older browsers might not implement mediaDevices at all, so we set an empty object first
+    if (navigator.mediaDevices === undefined) {
+      setMedia({})
+      console.log('No media devices available on this browser')
+    }
+    else {
+      setMedia(navigator.mediaDevices)
+      console.log(media)
+    }
+  }
+
   return (
-    <div className="webcamContainer">
-      <ColorGrid />
-      <Webcam
+    <div className="webcamContainer" onClick={getMediaDevices}>
+      {/* <ColorGrid /> */}
+      {/* <Webcam
         audio={false}
         screenshotFormat="image/jpeg"
         videoConstraints={videoConstraints}
@@ -34,7 +52,7 @@ const Camera = () => {
             <RadioButtonCheckedIcon sx={{ fontSize: 80 }} />
           </div>
         )}
-      </Webcam>
+      </Webcam> */}
     </div>
   )
 }
