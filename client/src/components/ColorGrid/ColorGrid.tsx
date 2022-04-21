b6fbdf20431239256cedd2c58e6cf7abca4bc79a
Steven Quintana <steve.e.quint@gmail.com>
2022-04-21 14:12:47 -0700

    Delete old color grid and make new one highest layer with transparent background and static text temporarily

--- client/src/components/ColorGrid/ColorGrid.tsx
+++ client/src/components/ColorGrid/ColorGrid.tsx
@@ -1,22 +1,9 @@
 import './ColorGrid.css'
 
-const displayColorBoxes: any = () => {
-  const photoWidth = window.innerWidth
-  const gridLength = photoWidth / 10
-  console.log(gridLength)
-
-  let photoBoxList = []
-  for (let length = 0; length < gridLength; length++) {
-    photoBoxList.push(<div className="box"></div>)
-  }
-
-  return photoBoxList
-}
-
 const ColorGrid = () => {
   return (
     <div className="grid">
-      {displayColorBoxes()}
+      <h1 className="colorText">Hello</h1>
     </div>
   )
 }
