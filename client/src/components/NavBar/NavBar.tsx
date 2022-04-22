b4bf33ffc08b33371f2f651ca14d6cd42ba615a6
Steven Quintana <steve.e.quint@gmail.com>
2022-04-22 09:23:46 -0700

    Update global route state in icon update function

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { useAppDispatch } from '../../app/hooks'
+import { updateDefinedRoute } from './definedRouteSlice'
 import { Link } from 'react-router-dom'
 import Box from '@mui/material/Box'
 import BottomNavigation from '@mui/material/BottomNavigation'
@@ -6,6 +8,7 @@ import BottomNavigationAction from '@mui/material/BottomNavigationAction'
 import CottageRounded from '@mui/icons-material/CottageRounded'
 import WavesRoundedIcon from '@mui/icons-material/WavesRounded'
 import './NavBar.css'
+import { updateAverageColor } from '../Camera/averageColorSlice'
 
 // Typescript interface to define prop type for the defined route in NavBar
 interface navbar {
@@ -13,15 +16,18 @@ interface navbar {
 }
 
 const NavBar: React.FC<navbar> = (props) => {
+  const dispatch = useAppDispatch()
   const [value, setValue] = useState(0)
 
   const updateIconClass = (icon: String) => {
     // Return a class string depending on the icon and definedRoute
     switch(icon) {
       case 'waize':
+        dispatch(updateAverageColor(icon))
         if (props.definedRoute === 'waize') { return 'activeLinkIcon' }
         else { return 'inactiveLinkIcon' }
       case 'waizeVR':
+        dispatch(updateAverageColor(icon))
         return props.definedRoute === 'waizeVR' ? 'activeLinkIcon' : 'inactiveLinkIcon'
       default:
         return 'inactiveLinkIcon'
