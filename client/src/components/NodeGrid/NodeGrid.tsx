4f85df3a312eb27e055039809cb590c412b060a7
Steven Quintana <steve.e.quint@gmail.com>
2022-05-05 15:12:48 -1000

    Create a function that loops through screen px potential value and draws a virtual node for each

--- client/src/components/NodeGrid/NodeGrid.tsx
+++ client/src/components/NodeGrid/NodeGrid.tsx
@@ -1,9 +1,23 @@
+import './NodeGrid.css'
 
 const NodeGrid = () => {
-  return (
-    <>
 
-    </>
+  // Create a display of 1px node that will mirror rgb nodes from the camera
+  const generateNodeGrid = () => {
+    
+    let gridArray = []
+
+    for (let index = 0; index < 243359; index++) {
+      gridArray.push(<div className="virtualNode"></div>)
+    }
+
+    return gridArray
+  }
+
+  return (
+    <div className="nodeGridContainer">
+      {generateNodeGrid}
+    </div>
   )
 }
 
